6d4c77866dba8f40cd4c9039d38f046fbca77d34
NK7 <51837694+FrostBreker@users.noreply.github.com>
2022-03-20 17:34:30 +0000

    change misc.js

--- Embeds/Misc.js
+++ Embeds/Misc.js
@@ -19,8 +19,8 @@ module.exports = {
     },
     rules2: () => {
         const embed = new MessageEmbed()
-            .setTitle("Règlement du serveur discord")
-            .setDescription("**Règle n°1** • Avoir une attitude correcte est adaptée pour former une atmosphère chaleureuse autour des membres.\n\n**Règle n°2** • Ne pas spam, ni flood, ni insulter.\n\n**Règle n°3** • Les pseudos/images de profils ne doivent pas entraîner à la haine ou à la violence.\n\n**Règle n°4** • Les caractères spéciaux, pornographiques, sexistes, homophobes… Sont interdits que ce soit à l'écrit ou bien dans les pseudos.\n\n**Règle n°5** • La pub est interdite sauf pour le rôle <@&952530060152295424> *(la pub en message privé est également interdite).*\n\n**Règle n°6** • Vous devez utilisez les salons selon leurs différentes utilités. Tout tentative d'arnaque est punissable.")
+            .setTitle("Règlement de Kayrox - Art")
+            .setDescription("**Règle n°1** • Avoir une attitude correcte et adaptée afin de conserver une atmosphère chaleureuse.\n\n**Règle n°2** • Merci de ne pas Spam, Flood, ou encore Insulter.\n\n**Règle n°3** • Les pseudos/images de profil ne doivent pas inciter à la haine ou à la violence.\n\n**Règle n°4** • Les caractères spéciaux, pornographiques, sexistes, homophobes… Sont interdits à l'écrit comme à travers des pseudos.\n\n**Règle n°5** • La pub est interdite sauf pour le rôle <@&952530060152295424> *(la pub en message privé est également interdite).*\n\n**Règle n°6** • Vous devez utiliser les salons selon leurs différentes utilités. Toute tentative d'arnaque est punissable.")
             .setColor(14003152)
             .setTimestamp()
 
@@ -29,7 +29,7 @@ module.exports = {
     rules3: () => {
         const embed = new MessageEmbed()
             .setTitle("Règlement des commandes")
-            .setDescription("**Règle n°1** • Toute pub mensongère envers le discord est punissable.\n\n**Règle n°2** • La publication d'avis sur un service n'étant pas constructive ou visant à nuire au bien être du discord est interdite.\n\n**Règle n°3** • Toute tentative de vol ou d'arnaque lors d'une commande est strictement interdite par ce présent règlement.")
+            .setDescription("**Règle n°1** • Toute pub mensongère envers le discord est punissable.\n\n**Règle n°2** • La publication d'avis sur un service n'étant pas constructive ou visant à nuire au bien être de Kayrox - Art est interdite.\n\n**Règle n°3** • Toute tentative de vol ou d'arnaque lors d'une commande est strictement interdite par ce présent règlement.")
             .setColor(14003152)
             .setTimestamp()
             .setFooter("Kayrox' Bot", 'https://www.zupimages.net/up/22/11/un0j.png');
